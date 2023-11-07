1a0e70179ae1a8e5eb2088c3c3cfd48722d6c37d
Matheo <matheo.pedron57@gmail.com>
2023-11-07 17:21:07 +0100

    Mise en place du design de la page login

--- src/vue/js/component.js
+++ src/vue/js/component.js
@@ -1,9 +1,27 @@
 window.addEventListener('load', () => {
-    let inputs = document.querySelectorAll('.input > input');
-    console.log(inputs);
-    inputs.forEach(input => {
-        input.addEventListener('input', () => {
-            input.setAttribute('value', input.value);
-        });
-    });
+	let inputs = document.querySelectorAll('.input > input');
+	inputs.forEach(input => {
+		input.addEventListener('input', () => {
+			input.setAttribute('value', input.value);
+		});
+	});
+
+	let password_inputs = document.querySelectorAll('.input.password');
+	password_inputs.forEach(input => {
+		input.querySelector('img').addEventListener('click', (event) => {
+			let el_input = input.querySelector('input');
+			if (event.target.src.includes('slash')) {
+				el_input.setAttribute('type', 'text');
+				event.target.title = 'Cacher le mot de passe';
+				event.target.src = 'images/eye.svg';
+			} else {
+				el_input.setAttribute('type', 'password');
+				event.target.title = 'Afficher le mot de passe';
+				event.target.src = 'images/eye-slash.svg';
+			}
+		});
+	});
+
+
+
 });
