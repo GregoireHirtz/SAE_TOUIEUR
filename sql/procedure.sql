bf781d77eecfeb88edd15546adfe952aa4eb67bd
LeMouj <enzo.mougin9@etu.univ-lorraine.fr>
2023-11-10 08:36:04 +0100

    Mise a jour procedure sql

--- sql/procedure.sql
+++ sql/procedure.sql
@@ -1,39 +1,72 @@
 -- DROP des procédures
-DROP PROCEDURE IF EXISTS `obtenirAbonnementUtilisateur`;
-DROP PROCEDURE IF EXISTS `obtenirAbonnementTag`;
-DROP PROCEDURE IF EXISTS `obtenirTouitesUtilisateur`;
-DROP PROCEDURE IF EXISTS `obtenirMeilleursTouites`;
-DROP PROCEDURE IF EXISTS ajoutPublier;
-DROP FUNCTION IF EXISTS `ajoutTouite`;
-DROP FUNCTION IF EXISTS ajoutImage;
-DROP FUNCTION IF EXISTS `ajoutTag`;
-DROP PROCEDURE IF EXISTS ajoutUtilisateur;
-DROP PROCEDURE IF EXISTS `ajoutUtiliserTag`;
-DROP PROCEDURE IF EXISTS ajoutUtiliserImage;
-DROP PROCEDURE IF EXISTS `obtenirUtilisateurAbo`;
-DROP PROCEDURE IF EXISTS `obtenirTouitesUtilisateursSuivis`;
-DROP PROCEDURE IF EXISTS `obtenirTouitesTagChoisi`;
-DROP PROCEDURE IF EXISTS `voter`;
-DROP PROCEDURE IF EXISTS `compterLikes`;
-DROP PROCEDURE IF EXISTS `compterDislikes`;
-DROP FUNCTION IF EXISTS `calculerNote`;
+
+DROP PROCEDURE IF EXISTS afficherHistoriqueUtilisateur;
 DROP PROCEDURE IF EXISTS afficherTouite;
 DROP PROCEDURE IF EXISTS afficherTouiteImages;
+DROP PROCEDURE IF EXISTS afficherTouiteAboTag;
+DROP PROCEDURE IF EXISTS afficherTouiteAboUtilisateur;
 DROP PROCEDURE IF EXISTS afficherTouiteTags;
-DROP PROCEDURE IF EXISTS retouiter;
+DROP FUNCTION IF EXISTS ajoutHistorique;
+DROP FUNCTION IF EXISTS ajoutImage;
+DROP PROCEDURE IF EXISTS ajoutPublier;
+DROP PROCEDURE IF EXISTS ajoutRecherche;
+DROP FUNCTION IF EXISTS `ajoutTag`;
+DROP FUNCTION IF EXISTS `ajoutTouite`;
+DROP PROCEDURE IF EXISTS ajoutUtilisateur;
+DROP PROCEDURE IF EXISTS ajoutUtiliserImage;
+DROP PROCEDURE IF EXISTS `ajoutUtiliserTag`;
+DROP PROCEDURE IF EXISTS annulerAbonnementTag;
+DROP PROCEDURE IF EXISTS annulerAbonnementUtilisateur;
+DROP PROCEDURE IF EXISTS annulerLikeDislike;
+DROP FUNCTION IF EXISTS `calculerNote`;
+DROP PROCEDURE IF EXISTS `compterDislikes`;
+DROP PROCEDURE IF EXISTS `compterLikes`;
+DROP FUNCTION IF EXISTS etreAboTag;
+DROP FUNCTION IF EXISTS etreAboUtilisateur;
+DROP FUNCTION IF EXISTS etreVote;
+DROP PROCEDURE IF EXISTS `obtenirAbonnementTag`;
+DROP PROCEDURE IF EXISTS `obtenirAbonnementUtilisateur`;
+DROP PROCEDURE IF EXISTS `obtenirMeilleursTouites`;
+DROP PROCEDURE IF EXISTS `obtenirTouitesTagChoisi`;
+DROP PROCEDURE IF EXISTS `obtenirTouitesUtilisateur`;
+DROP PROCEDURE IF EXISTS `obtenirTouitesUtilisateursSuivis`;
+DROP PROCEDURE IF EXISTS obtenirUsername;
+DROP PROCEDURE IF EXISTS obtenirUtilisateurAbo;
 DROP PROCEDURE IF EXISTS sabonnerTag;
 DROP PROCEDURE IF EXISTS sabonnerUtilisateur;
-DROP PROCEDURE IF EXISTS voir;
-DROP PROCEDURE IF EXISTS annulerLikeDislike;
 DROP PROCEDURE IF EXISTS supprimerTouite;
-DROP PROCEDURE IF EXISTS afficherHistoriqueUtilisateur;
-DROP FUNCTION IF EXISTS ajoutHistorique;
-DROP PROCEDURE IF EXISTS ajoutRecherche;
-DROP PROCEDURE IF EXISTS annulerAbonnementUtilisateur;
-DROP PROCEDURE IF EXISTS annulerAbonnementTag;
-
+DROP PROCEDURE IF EXISTS voir;
+DROP PROCEDURE IF EXISTS `voter`;
 
 -- Créations des PROCEDURES
+
+-- afficherHistoriqueUtilisateur
+CREATE PROCEDURE afficherHistoriqueUtilisateur (v_email TEXT)
+BEGIN
+    SELECT r.recherche, r.dateRecherche
+    FROM Historique h inner join Recherche r on h.idRecherche=r.idRecherche
+    where emailUt=v_email;
+end;
+
+-- Affichage pour touite
+CREATE PROCEDURE afficherTouite(v_idTouite INT)
+BEGIN
+    SELECT tou.texte, DATE_FORMAT(tou.date, '%d-%m-%Y à %H:%i') as formatted_date, tou.notePertinence,
+           tou.nbLike, tou.nbDislike, tou.nbVue, u.username
+    FROM Utilisateur u  INNER JOIN PublierPar p ON u.emailUt = p.emailUt
+                        INNER JOIN Touite tou ON p.idTouite = tou.idTouite
+    WHERE tou.idTouite = v_idTouite;
+END;
+
+-- Affichage des chemins des images d'un touite
+create procedure afficherTouiteImages(IN v_idTouite int)
+BEGIN
+    SELECT im.cheminSrc
+    FROM Touite tou INNER JOIN UtiliserImage ui ON tou.idTouite = ui.idTouite
+                    INNER JOIN Image im ON ui.idImage=im.idImage
+    WHERE tou.idTouite = v_idTouite;
+END;
+
 
 -- Liste des abonnements (utilisateurs)
 DELIMITER $$
@@ -232,14 +265,6 @@ SET nbVue = nbVue+1
 WHERE idTouite=v_idTouite;
 END;
 
--- Incrémenter retouite
-CREATE PROCEDURE retouiter(v_idTouite INT)
-BEGIN
-UPDATE Touite
-SET nbRetouite = nbRetouite+1
-WHERE idTouite=v_idTouite;
-END;
-
 -- Ajouter abonnement utilisateur
 CREATE PROCEDURE sabonnerUtilisateur(mail1 TEXT, mail2 TEXT)
 BEGIN
@@ -274,15 +299,7 @@ where ta.idTag = tagChoisi
 ORDER BY t.notePertinence DESC, t.date DESC$$
 DELIMITER ;
 
--- Affichage pour touite
-CREATE PROCEDURE afficherTouite(v_idTouite INT)
-BEGIN
-SELECT tou.texte, DATE_FORMAT(tou.date, '%d-%m-%Y à %H:%i') as formatted_date, tou.notePertinence,
-       tou.nbLike, tou.nbDislike, tou.nbVue, u.username
-FROM Utilisateur u  INNER JOIN PublierPar p ON u.emailUt = p.emailUt
-                    INNER JOIN Touite tou ON p.idTouite = tou.idTouite
-WHERE tou.idTouite = v_idTouite;
-END;
+
 
 -- Affichage des tags pour un touite
 CREATE PROCEDURE afficherTouiteTags(v_idTouite INT)
@@ -302,13 +319,7 @@ FROM Touite tou INNER JOIN UtiliserImage ui ON tou.idTouite = ui.idTouite
 WHERE tou.idTouite = v_idTouite;
 END;
 
--- afficherHistoriqueUtilisateur
-CREATE PROCEDURE afficherHistoriqueUtilisateur (v_email TEXT)
-BEGIN
-SELECT r.recherche, r.dateRecherche
-FROM Historique h inner join Recherche r on h.idRecherche=r.idRecherche
-where emailUt=v_email;
-end;
+
 
 
 -- ajoutRecherche;
@@ -341,19 +352,67 @@ WHERE emailUt = v_mail and idTag=v_idTag;
 end;
 
 -- afficher liste des touites abonnements utilisateurs (mail)
-
+CREATE PROCEDURE afficherTouitesAboUtilisateur(v_email TEXT)
+BEGIN
+select T.texte, T.date, T.notePertinence, T.nbLike, T.nbDislike, T.nbRetouite, T.nbVue, u.username
+from EtreAboUtilisateur aUt inner join Utilisateur u on aUt.emailUtAbo = u.emailUt
+                            inner join PublierPar PP on u.emailUt = PP.emailUt
+                            inner join Touite T on PP.idTouite = T.idTouite
+where aUt.emailUt=v_email;
+END;
 
 -- afficher liste des touites abonnements tags (mail)
-
+CREATE PROCEDURE afficherTouitesAboTag(v_email TEXT)
+BEGIN
+select DISTINCT t.texte, t.date, t.notePertinence, t.nbLike, t.nbDislike, t.nbRetouite, t.nbVue, u2.username
+from Utilisateur u inner join EtreAboTag aTa on u.emailUt = aTa.emailUt
+                   inner join Tag ta on aTa.idTag = ta.idTag
+                   inner join UtiliserTag ut on ta.idTag = ut.idTag
+                   inner join Touite t on ut.idTouite = t.idTouite
+                   inner join PublierPar p on t.idTouite = p.idTouite
+                   inner join Utilisateur u2 on p.emailUt=u2.emailUt
+where p.emailUt != v_email and aTa.idTag IN (select DISTINCT ta.idTag
+                                                            from EtreAboTag aTa inner join Tag ta on aTa.idTag = ta.idTag
+                                                                                inner join UtiliserTag ut on ta.idTag = ut.idTag
+                                                                                inner join Touite t on ut.idTouite = t.idTouite
+                                                            where aTa.emailUt =v_email);
+END;
 
 -- etreAbonnéUtilisateur (mail mail)
-
+CREATE FUNCTION etreAboUtilisateur(v_email TEXT, v_emailAbo TEXT) RETURNS bool
+BEGIN
+    DECLARE v_etreAbo date;
+select dateAboUt into v_etreAbo from EtreAboUtilisateur where emailUt=v_email and emailUtAbo=v_emailAbo;
+IF (v_etreAbo) IS NULL THEN
+        return false;
+ELSE
+        return true;
+end if;
+end;
 
 -- etreAbonnéTag (mail idtag)
-
+CREATE FUNCTION etreAboTag(v_email TEXT, v_idTag INT) RETURNS bool
+BEGIN
+    DECLARE v_etreAbo date;
+select dateAboTag into v_etreAbo from EtreAboTag where emailUt=v_email and idTag=v_idTag;
+IF (v_etreAbo) IS NULL THEN
+        return false;
+ELSE
+        return true;
+end if;
+end;
 
 -- etreVote (idtouite, user)
-
+CREATE FUNCTION etreVote(v_idTouite INT, v_mail TEXT) RETURNS bool
+BEGIN
+    DECLARE v_etreVote date;
+select vote into v_etreVote from AvoirVote where emailUt=v_mail and idTouite=v_idTouite;
+IF (v_etreVote) IS NULL THEN
+        return false;
+ELSE
+        return true;
+end if;
+end;
 
 
 -- obtenir utilisateur avec touite
